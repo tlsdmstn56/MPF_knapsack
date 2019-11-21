be99a1171b0edb25a13519c21938a56787344a01
Eunsoo Sheen <eunsoo.sheen@gmail.com>
2019-11-22 01:26:37 +0900

    parallel-copa 추가

---
+++ src/parallel-copa/parallel_copa.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include <thread>
+#include <chrono>
+
+
+#include <CL/cl.h>
+
+#include <common/Data.hpp> // Data
+#include <common/DataGenerator.hpp> // DataGenerator
+#include <common/Timer.hpp> // Timer
+
+#include "CLKernels.hpp"
+
+int main() {
+    using namespace std::chrono_literals;
+    
+    Timer timer;
+    timer.start();
+    Data data = DataGenerator::generate(10, 43);
+    std::cout << "(weight, price)\n";
+    for(const auto& e: data.getTable()) {
+        std::cout << "(" << e.first << ", " << e.second << ")\n";
+    }
+    std::this_thread::sleep_for(2s);
+    std::cout << timer.stop() << "\n";
+    return 0;
+}
